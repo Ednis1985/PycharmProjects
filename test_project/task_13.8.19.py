41e6ea0d12ca4a9b314e8cb0a100c9aa2669f268
Ednis1985 <denis.nazarov@ajhackett.com>
2022-05-27 21:11:27 +0300

    update3

--- test_project/task_13.8.19.py
+++ test_project/task_13.8.19.py
@@ -6,12 +6,12 @@ while True:
         if type(ticket_number) == int:
             break
     except ValueError:
-        print('Введите целое число')
+        print('Укажите возраст')
 for i in range(ticket_number):
     i += 1
     while True:
         try:
-            age_ticket = input(f'Для какого возраста билет №{i}? ')
+            age_ticket = input(f'Укажите возраст посетителя №{i}? ')
             age_ticket = int(age_ticket)
             if age_ticket < 18:
                 print('Билет бесплатный')
@@ -24,7 +24,7 @@ for i in range(ticket_number):
             if type(age_ticket) == int:
                 break
         except ValueError:
-            print('Введите целое число')
+            print('Укажите возраст')
 if ticket_number > 3:
     price = price - ((price / 100) * 10)
     print(f'Сумма к оплате {price} руб. с учетом дополнительной скидки на полную стоимость заказа 10%')
