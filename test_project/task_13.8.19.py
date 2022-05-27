15f47794a39961170e5e9956da32c7108ced98cb
Ednis1985 <denis.nazarov@ajhackett.com>
2022-05-27 21:07:42 +0300

    update2

--- test_project/task_13.8.19.py
+++ test_project/task_13.8.19.py
@@ -1,4 +1,4 @@
-price_all = 0
+price = 0
 while True:
     try:
         ticket_number = input('Количество билетов, которые Вы хотите приобрести на мероприятие? ')
@@ -11,22 +11,22 @@ for i in range(ticket_number):
     i += 1
     while True:
         try:
-            age_for_ticket = input(f'Для какого возраста билет №{i}? ')
-            age_for_ticket = int(age_for_ticket)
-            if age_for_ticket < 18:
+            age_ticket = input(f'Для какого возраста билет №{i}? ')
+            age_ticket = int(age_ticket)
+            if age_ticket < 18:
                 print('Билет бесплатный')
-            elif 25 > age_for_ticket >= 18:
-                price_all += 990
+            elif 25 > age_ticket >= 18:
+                price += 990
                 print('Стоимость билета: 990 руб.')
             else:
-                price_all += 1390
+                price += 1390
                 print('Стоимость билета: 1390 руб.')
-            if type(age_for_ticket) == int:
+            if type(age_ticket) == int:
                 break
         except ValueError:
             print('Введите целое число')
-if ticket_number > 5:
-    price_all = price_all - ((price_all / 100) * 20)
-    print(f'Сумма к оплате {price_all} руб. с учетом 20%-ой скидки на полную стоимость заказа за регистрацию больше 5-и человек')
+if ticket_number > 3:
+    price = price - ((price / 100) * 10)
+    print(f'Сумма к оплате {price} руб. с учетом дополнительной скидки на полную стоимость заказа 10%')
 else:
-    print(f'Сумма к оплате {price_all} руб.')
+    print(f'Сумма к оплате {price} руб.')
